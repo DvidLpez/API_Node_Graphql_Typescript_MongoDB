5599b3b51be333dd14f2ff9669271b2f31c7b895
David <xclavox@gmail.com>
2019-12-25 20:25:58 +0100

    fixed untracked files

--- dist/config/environments.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environments = {
-    conectionDB: {
-        protocol: 'mongodb://',
-        host: 'localhost:27017',
-        db: 'mongodbgraphql'
-    }
-};
